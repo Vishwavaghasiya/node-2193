c71794814b4101da280b1bce36aae24db33883b9
vishu vaghasiya <vishwavaghasiya595@gmail.com>
2023-07-24 10:24:41 +0530

    commited

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 // // fs module
-var http = require('http');
-var fs = require('fs');
+// var http = require('http');
+// var fs = require('fs');
 
-console.log("server");
-http.createServer(function(req,res){
-    fs.readFile("index.html",function(err,data){
-            res.writeHead(200 , {"content-type" : "text/html"});
-            res.write(data);
-            return res.end();  
-    })
-}).listen(8082);
+// console.log("server");
+// http.createServer(function(req,res){
+//     fs.readFile("index.html",function(err,data){
+//             res.writeHead(200 , {"content-type" : "text/html"});
+//             res.write(data);
+//             return res.end();  
+//     })
+// }).listen(8082);
 
 // // express using server 
 var express = require('express');
 var app = express();
 
-console.log('welcome to');
+console.log('hello ! server');
 app.get('/' , (req,res) => {
     res.send('express server is running now')
     console.warn('now you are using express');
